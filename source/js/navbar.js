51e621db663dc280a1273e47364ea0866b511e00
Hopperys <vano_ust.kz@mail.ru>
2021-04-06 21:36:43 +0600

    Сделал открытие меню + js

---
+++ source/js/navbar.js
@@ -0,0 +1,14 @@
+  var navMain = document.querySelector('.main-navbar');
+  var navToggle = document.querySelector('.main-navbar__burger');
+
+  navMain.classList.remove('main-navbar--nojs');
+
+  navToggle.addEventListener('click', function() {
+    if (navMain.classList.contains('main-navbar--closed')) {
+      navMain.classList.remove('main-navbar--closed');
+      navMain.classList.add('main-navbar--opened');
+    } else {
+      navMain.classList.add('main-navbar--closed');
+      navMain.classList.remove('main-navbar--opened');
+    }
+  });
