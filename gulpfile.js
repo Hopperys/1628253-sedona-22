9c88699cf90f52d1a4e542ce7711f261d1926651
Hopperys <vano_ust.kz@mail.ru>
2021-04-20 13:53:27 +0600

    Доделал сборку

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ const csso = require("gulp-csso");
 const rename = require("gulp-rename");
 const terser = require("gulp-terser");
 const imagemin = require("gulp-imagemin");
+const svgstore = require("gulp-svgstore");
+const del = require("del");
 const webp = require('gulp-webp');
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
@@ -87,12 +89,46 @@ const createWebp = () => {
 
 exports.createWebp = createWebp;
 
+// Sprite
+
+const sprite = () => {
+  return gulp.src("source/img/icons/sprites/*.svg")
+    .pipe(svgstore({
+      inlineSVG: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
+}
+
+exports.sprite = sprite;
+
+// Copy
+
+const copy = (done) => {
+  gulp.src([
+    "source/fonts/*.{woff2,woff}",
+    "source/*.ico",
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("build"))
+  done();
+}
+
+exports.copy = copy;
+
+// Clean
+
+const clean = () => {
+  return del("build");
+};
+
 // Server
 
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: "build"
     },
     cors: true,
     notify: false,
@@ -103,13 +139,59 @@ const server = (done) => {
 
 exports.server = server;
 
+// Reload
+
+const reload = (done) => {
+  sync.reload();
+  done();
+}
+
+exports.reload = reload;
+
 // Watcher
 
 const watcher = () => {
   gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
-  gulp.watch("source/*.html").on("change", sync.reload);
+  gulp.watch("source/js/*.js", gulp.series("script"));
+  gulp.watch("source/*.html", gulp.series("html"));
 }
 
 exports.default = gulp.series(
   styles, server, watcher
 );
+
+// Build
+
+const build = gulp.series(
+  clean,
+  copy,
+  optimizeImages,
+  gulp.parallel (
+    styles,
+    html,
+    script,
+    sprite,
+    createWebp
+  )
+)
+
+exports.build = build;
+
+// Default
+
+exports.default = gulp.series(
+  clean,
+  copy,
+  copyImages,
+  gulp.parallel(
+    styles,
+    html,
+    script,
+    sprite,
+    createWebp
+  ),
+  gulp.series(
+    server,
+    watcher
+  )
+)
